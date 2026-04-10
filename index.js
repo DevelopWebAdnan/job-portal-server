9edf8e8b38fd98fde9d7deadb39e6148093bb4c6
DevelopWebAdnan <adnaniqbal023@yahoo.com>
2026-04-10 22:52:01 +0600

    Create job detail route and get job detail data.

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express');
- const cors = require('cors');
- const app = express();
- require('dotenv').config()
- const port = process.env.PORT || 5000;
- const { MongoClient, ServerApiVersion } = require('mongodb');
+const cors = require('cors');
+const app = express();
+require('dotenv').config()
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
- app.use(express.json());
- app.use(cors());
+app.use(express.json());
+app.use(cors());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wy5hpga.mongodb.net/?appName=Cluster0`;
@@ -30,9 +30,16 @@ async function run() {
 
     const jobsCollection = client.db('jobPortal').collection('jobs');
 
-    app.get('/jobs', async(req, res) => {
+    app.get('/jobs', async (req, res) => {
       const cursor = jobsCollection.find();
       const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    app.get('/jobs/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await jobsCollection.findOne(query);
       res.send(result);
     })
 
@@ -45,10 +52,10 @@ async function run() {
 run().catch(console.dir);
 
 
- app.get('/', (req, res) => {
-    res.send('Jobs are falling from the sky.');
- })
+app.get('/', (req, res) => {
+  res.send('Jobs are falling from the sky.');
+})
 
- app.listen(port, () => {
-    console.log(`Jobs are waiting at port: ${port}`);
- })
+app.listen(port, () => {
+  console.log(`Jobs are waiting at port: ${port}`);
+})
