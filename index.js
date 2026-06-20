9c31aa77c4ebedeac7342fee8da6702f88a36179
DevelopWebAdnan <adnaniqbal023@yahoo.com>
2026-06-20 20:23:27 +0600

    Sorted job data by salary from server using sort()

--- index.js
+++ index.js
@@ -9,7 +9,11 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(express.json());
 app.use(cors({
-  origin: ["http://localhost:5173"],
+  origin: [
+    "http://localhost:5173",
+    "https://simple-firebase-5adbe.web.app",
+    "https://simple-firebase-5adbe.firebaseapp.com"
+  ],
   credentials: true
 }));
 app.use(cookieParser());
@@ -20,14 +24,14 @@ const logger = (req, res, next) => {
 }
 
 const verifyToken = (req, res, next) => {
-  // console.log('Inside verify token middleware', req.cookies);
 
-  const token = req?.cookies?.token;
+  const token = req.cookies?.token;
 
   if (!token) {
     return res.status(401).send({ message: "Unauthorized access" })
   }
 
+  // verify the token
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
     if (err) {
       return res.status(401).send({ message: "Unauthorized access" })
@@ -51,10 +55,10 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
     const jobsCollection = client.db('jobPortal').collection('jobs');
     const jobApplicationsCollection = client.db('jobPortal').collection('job_applications');
@@ -62,11 +66,12 @@ async function run() {
     // Auth related APIs
     app.post('/jwt', async (req, res) => {
       const user = req.body;
-      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '5h' });
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '10h' });
       res
         .cookie('token', token, {
           httpOnly: true,
-          secure: false, // in localhost
+          secure: process.env.NODE_ENV === "production",
+          sameSite: process.env.NODE_ENV === "production" ? "none" : "strict"
         })
         .send({ success: true });
     })
@@ -75,19 +80,29 @@ async function run() {
       res
         .clearCookie('token', {
           httpOnly: true,
-          secure: false,
+          secure: process.env.NODE_ENV === "production",
+          sameSite: process.env.NODE_ENV === "production" ? "none" : "strict"
         })
         .send({ success: true });
     })
     // jobs related APIs
-    app.get('/jobs', logger, async (req, res) => {
-      console.log('Now inside the api callback');
+    app.get('/jobs', async (req, res) => {
+      // console.log('Now inside the api callback', req.query);
+      console.log(req.query);
       const email = req.query.email;
-      let query = {}
+
+      const sort = req.query?.sort;
+
+      let query = {};
+      let sortedQuery = {};
       if (email) {
         query = { hr_email: email }
       }
-      const cursor = jobsCollection.find(query);
+      
+      if (sort == "true") {
+        sortedQuery = { "salaryRange.min": -1 }
+      }
+      const cursor = jobsCollection.find(query).sort(sortedQuery);
       const result = await cursor.toArray();
       res.send(result);
     })
@@ -110,11 +125,14 @@ async function run() {
     app.get('/job-application', verifyToken, async (req, res) => {
       const email = req.query.email;
       const query = { applicant_email: email };
-      const result = await jobApplicationsCollection.find(query).toArray()
 
+      // token email !== query email
       if (req.user.email !== req.query.email) {
         return res.status(403).send({ message: 'forbidden access' })
       }
+
+      const result = await jobApplicationsCollection.find(query).toArray()
+
 
       // poor way to aggregate data
       for (const application of result) {
